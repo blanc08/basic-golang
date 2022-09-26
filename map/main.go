538a81f5de97aca14df7ac62c3e91de59b1bc849
Bagus <bagusoktaviadi1@gmail.com>
2022-09-26 23:26:38 +0700

    Slice Map

--- map/main.go
+++ map/main.go
@@ -12,15 +12,29 @@ func main() {
 	// myMap["Ruby"] = 1
 
 	// * direct
-	myMap := map[string]float64{"a": 1.2, "b": 2.2}
+	// myMap := map[string]float64{"a": 1.2, "b": 2.2}
 
-	for _, v := range myMap {
-		fmt.Println(v)
+	// * for loop
+	// for _, v := range myMap {
+	// fmt.Println(v)
+	// }
+
+	// * check existence
+	// value, isAvailable := myMap["c"]
+	// fmt.Print(value, isAvailable)
+	// fmt.Println(myMap)
+
+	// * slice map
+	students := []map[string]string{
+		{"name": "Agung", "score": "A"},
+		{"name": "Ilham", "score": "B"},
+		{"name": "Aris", "score": "C"},
 	}
 
-	// check existence
-	value, isAvailable := myMap["c"]
-	fmt.Print(value, isAvailable)
-	fmt.Println(myMap)
+	for _, values := range students {
+		fmt.Println(values["name"])
+	}
+
+	fmt.Println(students)
 
 }
